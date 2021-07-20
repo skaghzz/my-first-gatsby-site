db8120c135743682d4e43d76a4beaad458958f1d
namhokim <skaghzz@naver.com>
2021-07-20 10:41:07 +0900

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,9 +5,14 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-gatsby-cloud",
-    'gatsby-plugin-image',
-    'gatsby-plugin-sharp',
-    //'gatsby-source-filesystem',
-    //'gatsby-transformer-sharp'
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    }
   ],
 };
